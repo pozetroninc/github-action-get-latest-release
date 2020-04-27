6ac0f7bb4f443a8b3c7cfa5274fee8fb2d3cb823
Pozetron Inc <neil@pozetroninc.com>
2020-04-26 17:37:15 -0700

    Initial action

---
+++ main.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+const { Octokit } = require("@octokit/rest");
+
+const owner = core.getInput('owner');
+const repo = core.getInput('repo');
+
+const octokit = new Octokit()
+
+async function run() {
+    try {
+        const releases  = await octokit.repos.listReleases({
+            owner: owner,
+            repo: repo,
+            });
+        core.setOutput('release', releases.data[0].tag_name)
+        }
+    catch (error) {
+        core.setFailed(error.message);
+        }
+    }
+
+run()
