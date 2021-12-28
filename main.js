63f55e19df7dbfbbe6a773a2c99ef4edc3f80979
Sascha Depold <sdepold@users.noreply.github.com>
2021-12-28 13:16:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -25,9 +25,8 @@ async function run() {
             releases = releases.filter(x => x.draft != true);
         }
         if (releases.length) {
-            core.info(JSON.stringify(releases[0]))
-            core.setOutput('release', releases[0].tag_name)
-            core.setOutput('id', releases[0].id)
+            core.setOutput('release', releases[0].tag_name);
+            core.setOutput('id', String(releases[0].id));
         } else {
             core.setFailed("No valid releases");
         }
